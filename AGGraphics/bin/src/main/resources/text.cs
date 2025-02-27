eb3979f04509b27ab6eed695068d2617059054a6
KartoffelL <84570564+KartoffelL@users.noreply.github.com>
2025-02-27 14:56:33 +0100

    Delete AGGraphics/bin directory
    removed '.bin' folder

--- AGGraphics/bin/src/main/resources/text.cs
+++
@@ -1,9 +0,0 @@
-#version 460 core
-
-layout(binding = 0) uniform UniformBufferObject {float time;mat4 view;} ubo;
-
-layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
-
-void main() {
-	
-}
