da8bbe8c6855bcb385184154350d4cc158b9731d
Kartoffel_Licht <k.athamneh@web.de>
2025-02-27 14:41:48 +0100

    test (updated javadoc)

--- AGGraphics/src/main/java/Kartoffel/Licht/AGGraphics/AGCanvas.java
+++ AGGraphics/src/main/java/Kartoffel/Licht/AGGraphics/AGCanvas.java
@@ -6,13 +6,25 @@ import Kartoffel.Licht.Vulkan.Framebuffer;
 import Kartoffel.Licht.Vulkan.RenderPass;
 
 /**
- * 
+ * An {@link AGCanvas} is an interface for handling {@link RenderPass}es and {@link Framebuffer}s, etc..
  */
 public interface AGCanvas {
 	
-	
+	/**
+	 * {@return the RenderPass}
+	 */
 	public RenderPass getRenderpass();
+	/**
+	 * Returns the colors the destination may get cleared with.
+	 * @param index the index.
+	 * @return the clear colors (RGBA) of every image
+	 */
 	public Vector4f[] getClearColors(int index);
+	/**
+	 * Returns the {@link Framebuffer} this canvas links to, given the index.
+	 * @param index the index
+	 * @return the {@link Framebuffer} object.
+	 */
 	public Framebuffer getFramebuffer(int index);
 
 }
