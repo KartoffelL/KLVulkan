1322ad65b2e46f22e7493677e2fabede6f14df3e
KartoffelL <84570564+KartoffelL@users.noreply.github.com>
2025-02-27 14:36:47 +0100

    Delete KLVulkan/target directory
    Gitnore cleanup

--- KLVulkan/target/reports/apidocs/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"KLVulkan"}];updateSearchResults();
--- KLVulkan/target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"KLVulkan","l":"Kartoffel.Licht.Vulkan"},{"m":"KLVulkan","l":"Kartoffel.Licht.Vulkan.Impls"},{"m":"KLVulkan","l":"Kartoffel.Licht.Vulkan.video"}];updateSearchResults();
